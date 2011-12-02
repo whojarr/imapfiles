53ee0298c81d73dfb5eb35e63b71d9a1b448bdb1
whojarr <dhunter@pl.net>
2011-12-02 16:02:34 +1300

    updated contact and licence info to point at new file LICENSE.txt in README

--- imapfiles.py
+++ imapfiles.py
@@ -20,7 +20,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
-
 This script will connect to an imap server and download the attachements from a specified folder
 then archive them to a specified folder.
 """
